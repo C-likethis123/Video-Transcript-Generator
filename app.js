402872f62a44ad7e2fa827de750e9a79cd6647d7
Christina <chowjiaying211@gmail.com>
2019-08-10 09:07:21 +0800

    Initial app

---
+++ app.js
@@ -0,0 +1,24 @@
+﻿var express = require('express');
+var app = new express();
+var port = 3000;
+
+app.listen(port, function(err) {
+    if (typeof(err) == "undefined") {
+        console.log("Your application is running on " + port + " port");
+    }
+});
+
+app.use(express.static('public')); //set public directory to default directory
+app.set('views', './src/views'); //tell express where we store our views
+app.set('view engine', 'ejs'); //tell express which template engine we are using
+
+var transcriptRouter = require('./src/routes/transcriptRoute')();
+
+app.use('/transcript', transcriptRouter); //using the routers
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        title:'Lecture Video Transcript Generator'
+    });
+}); //displaying home page
+
---
+++ src/routes/transcriptRoute.js
@@ -0,0 +1,14 @@
+﻿var express=require('express');
+
+var texteditor=express.Router();
+var texteditor_router=function() {
+
+	texteditor.route("/")
+		.get(function(req, res) {
+				res.render('transcript', {title: 'Lecture Video Transcript Generator'});
+
+			});				
+
+	return texteditor;
+};
+module.exports=texteditor_router;
